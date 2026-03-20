b6deff31c2d51ec7b1ce22ce5f888c79eabe583d
Emilio Domínguez <emily@wonderly.com>
2026-03-20 20:50:33 -0300

    feat(eslint): Add React version setting to ESLint configuration Update commitlint configuration for improved rule formatting and readability

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,6 +4,22 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 120],
     'subject-case': [2, 'always', 'sentence-case'],
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
   },
-}
+};
